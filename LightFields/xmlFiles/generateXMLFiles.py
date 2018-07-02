5759fe088bd60bd73adbc7a895d20ec7aadb6f62
sudarshan <sudarshannagesh90@gmail.com>
2018-07-01 23:12:47 -0500

    Added f5Files

--- LightFields/xmlFiles/generateXMLFiles.py
+++ LightFields/xmlFiles/generateXMLFiles.py
@@ -3,9 +3,8 @@ import xml.dom.minidom
 import subprocess 
 import os
 import imageio
-import sys
-sys.path.insert(0, '/home/snagesh/git/OptimizationDeepLearningImageProcessing/')
-from LightFields.utils import data as data_utils
+import h5py
+import numpy as np 
 
 def createXMLstring(filename,scaleVal,cameraPosX,cameraPosY):
 	scene = etree.Element("scene",version="0.5.0")
@@ -30,19 +29,25 @@ def createXMLstring(filename,scaleVal,cameraPosX,cameraPosY):
 	reparsed_pretty = reparsed.toprettyxml(indent=" " * 4)
 	return reparsed_pretty
 
-#filenames = ["airboat","al","alfa147","cessna","cube","diamond","dodecahedron","gourd","humanoid_quad","humanoid_tri","icosahedron","lamp","magnolia","minicooper","octahedron","power_lines","roi","sandal","shuttle","skyscraper","slot_machine","teapot","tetrahedron","violin_case"]
-filenames = ["airboat"]
+def create_h5(data, label, path, file_name):
+
+	with h5py.File(os.path.join(path, file_name), 'w') as file:
+		file.create_dataset("data", data = data)
+		file.create_dataset("label", data = label)
+
+
+filenames = ["airboat","al","alfa147","cessna","cube","diamond","dodecahedron","gourd","humanoid_quad","humanoid_tri","icosahedron","lamp","magnolia","minicooper","octahedron","power_lines","roi","sandal","shuttle","skyscraper","slot_machine","teapot","tetrahedron","violin_case"]
 scaleVal  = [0.5,0.5,0.01,0.08,0.5,0.01,0.5,0.5,0.1,0.1,0.5,0.2,0.025,0.01,0.5,0.07,0.02,0.2,0.1,0.03,0.1,0.01,0.5,0.5]
 index = 0
 cameraPosOrigin = [5,1,-3]
 deltaCam = 0.1
 hr_image = []
 lr_image = []
-destination_path     = "/home/snagesh/git/OptimizationDeepLearningImageProcessing/LightFields/h5Files/"
-dataset_name         = ["generatedLightFields"]
+destination_path     = "/home/sudarshan/git/OptimizationDeepLearningImageProcessing/LightFields/h5Files/"
+dataset_name         = "generatedLightFields"
 
 for filename in filenames:
-	HRIndex  = 0
+	HRindex  = 0
 	with imageio.get_writer(filename+"/"+filename+".gif", mode='I') as writer:
 		for indx in range(-2,3):
 			for indy in range(-2,3):
@@ -57,12 +62,13 @@ for filename in filenames:
 				cmd = ["mitsuba", filename+"/"+filename+str(indx)+str(indy)+".xml"]    
 				cmd_out = subprocess.check_output(cmd)
 				image = imageio.imread(filename+"/"+filename+str(indx)+str(indy)+".png")
-				hr_image[:,:,:,index,HRindex] = np.asarray(image)
+				hr_image.append(np.asarray(image))
 				HRindex = HRindex+1
 				if indx == 0 and indy == 0: 
-					lr_image[:,:,:,index] = np.asarray(image)
+					lr_image.append(np.asarray(image))
 				writer.append_data(image)
+	print(["Completed index: "+str(index)])				
 	index = index+1
 
-data_utils.create_h5(data = lr_image, label = hr_image, path = destination_path, file_name = dataset_name+"training.h5")
-print("data of shape ", lr_image.shape, "and label of shape ", hr_image.shape, " created of type ", lr_image.dtype)
+create_h5(data = lr_image, label = hr_image, path = destination_path, file_name = dataset_name+"training.h5")
+print("data of length ", len(lr_image), "and label of length ", len(hr_image))
